1e9a58136b11bae5e6711d8d0a6b283d45b6b961
JasmineYil <71649452+JasmineYil@users.noreply.github.com>
2021-05-16 15:17:02 +0200

    small changes

--- src/main/java/at/ac/fhcampuswien/CompositePatternDemo.java
+++ src/main/java/at/ac/fhcampuswien/CompositePatternDemo.java
@@ -6,13 +6,13 @@ public class CompositePatternDemo {
 	
 	{
 		
-		 Employee emp1=new Cashier(101,"Sohan Kumar", 20000.0);
+		 Employee emp1=new Cashier(101,"Alexander Pruka", 20000.0);
 		 
-		 Employee emp2=new Cashier(102,"Mohan Kumar", 25000.0);
+		 Employee emp2=new Cashier(102,"Jasmine Yilmaz", 25000.0);
 		 
-		 Employee emp3=new Accountant(103,"Seema Mahiwal", 30000.0); 
-		 
-		 Employee manager1=new BankManager(100,"Ashwani Rajput",100000.0);
+		 Employee emp3=new Accountant(103,"Sahani Peiris", 30000.0);
+
+		 Employee manager1=new BankManager(100,"Clarissa Treml",100000.0);
 		  
 		  manager1.add(emp1);
 		  manager1.add(emp2);
